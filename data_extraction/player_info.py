75bc58bf0f516488bfeb1d06434cff301115b178
Maxwell Coleman <mac488@cornell.edu>
2020-05-02 17:28:42 -0400

    Work

--- data_extraction/player_info.py
+++ data_extraction/player_info.py
@@ -2,16 +2,29 @@ from bs4 import BeautifulSoup
 import re
 import pandas as pd
 
-in_filename = "web/players/AUDL_player_info.html"
+team_column = []
+player_name = []
+
 out_filename = "data/player_info.csv"
+teams = pd.read_csv("data/teams.csv")
+for team in teams['link']:
 
-f = open(in_filename, "r").read()
-soup = BeautifulSoup(f, "lxml")
-f.close()
+    in_filename = "web/players" + team + ".html"
 
-general = soup.find("ul", class_= "views-fluid-grid-list").find_all("li")
-results = {}
-for li in general :
-    name = li.find("div", class_="views-field-player-field-player-display-name").find("span").get_text()
-    number = li.find("div", class_="views-field-player-field-jersey-number").find("span").get_text()
-    results[name] = number
+
+    f = open(in_filename, "r").read()
+    soup = BeautifulSoup(f, "lxml")
+
+    general = soup.find("ul", class_= "views-fluid-grid-list")
+    general = general.find_all("li")
+    for li in general :
+        name = li.find("div", class_="views-field-player-field-player-display-name").find("span").get_text()
+
+        player_name.append(name)
+
+        team_column.append(team)
+
+data = [team_column, player_name]
+
+df = pd.DataFrame(data)
+df.to_csv(out_filename)
