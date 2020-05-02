735d108e2b0f02ddeb8b1d8f73486c60269e6ca7
Maxwell Coleman <mac488@cornell.edu>
2020-05-02 14:29:03 -0400

    Have some questions but hopefully did my full part

--- data_extraction/player_data_parser.py
+++ data_extraction/player_data_parser.py
@@ -14,3 +14,12 @@ single_player = soup.find_all('tr', class_ = "views-field-tooltip-row")
 stat_names = soup.find_all('tr', class_ = "views-label2-tooltip-row-0")
 for name in stat_names:
     print(name.find('a').get_text())
+
+
+# general = .find(‘ul’, class_= ‘views-fluid-grid-list’).find_all(‘li’);
+# results = {}
+# For li in general{
+# name = li.find(‘div’, class_=’views-field-player-field-player-display-name’).find(‘span’).get_text()
+# number = li.find(‘div’, class_=’views-field-player-field-jersey-number’).find(‘span’).get_text()
+# Results[name] = number
+# }
